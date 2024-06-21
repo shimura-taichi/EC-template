55acb3a30cc3288cb46551dd8c7d1e52db4cf733
shimura-taichi <f7c-f13l-tnmfmf@outlook.jp>
2024-06-21 17:43:40 +0900

    ECサイト初級授業3時間目済

--- template/sql/ecsite.sql
+++ template/sql/ecsite.sql
@@ -1,25 +1,30 @@
 set names utf8;
-set foreign_key_checks = 0;
+set foreign_key_checks=0;
 drop database if exists ecsite;
 create database if not exists ecsite;
 use ecsite;
 
 drop table if exists login_user_transaction;
 
-create table login_user_transaction(id int not null primary key auto_increment,
-login_id varchar(16) unique,login_pass varchar(16),user_name varchar(50),insert_datedatetime,
-updated_date datetime);
+create table login_user_transaction(id int not null primary key auto_increment,login_id varchar(16)unique,
+login_pass varchar(16),
+user_name varchar(50),
+insert_date time,
+updated_date time);
 
 
 drop table if exists item_info_transaction;
-create table item_info_transaction(
- id int not null primary key auto_increment,item_name varchar(30),
- item_priceint,item_stockint,insert_datedatetime,update_date datetime);
+
+create table item_info_transaction(id int not null primary key auto_increment,item_name varchar(30),
+item_price int,item_stock int,
+insert_date time,
+update_date time);
 
 drop table if exists user_buy_item_transaction;
+
 create table user_buy_item_transaction(
- id int not null primary key auto_increment,item_transaction_id int,total_priceint,
- total_count int,user_master_idvarchar(16),pay varchar(30),insert_datedatetime,delete_date datetime);
+id int not null primary key auto_increment,item_transaction_id int,total_price int,
+total_count int,user_master_id varchar(16),pay varchar(30),insert_date time,delete_date time);
 
 INSERT INTO item_info_transaction(item_name,item_price, item_stock) VALUES("ノートBook",100,50);
 INSERT INTO login_user_transaction(login_id,login_pass, user_name) VALUES("internous","internous01","test");
