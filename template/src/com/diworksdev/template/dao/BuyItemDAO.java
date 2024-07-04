daa5ba8d04e48c0cc295bcb7e9d07c2fbdd62123
shimura-taichi <f7c-f13l-tnmfmf@outlook.jp>
2024-07-04 22:28:34 +0900

    初級サイト授業5時間目済

--- template/src/com/diworksdev/template/dao/BuyItemDAO.java
+++ template/src/com/diworksdev/template/dao/BuyItemDAO.java
@@ -13,7 +13,7 @@ public class BuyItemDAO {
 		 Connection connection = dbConnector.getConnection();
 		 BuyItemDTO buyItemDTO = new BuyItemDTO();
 
-		 String sql = "SELECT id, item_name, item_price FROM item_info_transaction";
+		 String sql = "SELECT id,item_name,item_price FROM item_info_transaction";
 
 		 try{
 			 PreparedStatement preparedStatement = connection.prepareStatement(sql);
